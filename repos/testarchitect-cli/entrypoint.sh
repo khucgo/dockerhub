8e3b1ec0bc6b0fbf924f2d1d6a4ad30b09959325
QV <qv@localhost.local>
2021-12-02 14:47:23 +0700

    testarchitect-cli

--- repos/testarchitect-cli/entrypoint.sh
+++ repos/testarchitect-cli/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-echo $TA_TIMEOUT
+
 echo $TA_EXECUTE
-echo "timeout -k $TA_TIMEOUT $TA_TIMEOUT ta execute $TA_EXECUTE" > ta_exec.sh
+
+if [ -n "$TA_TIMEOUT" ]
+then
+  echo $TA_TIMEOUT
+  echo "timeout -k $TA_TIMEOUT $TA_TIMEOUT ta execute $TA_EXECUTE" > ta_exec.sh
+else
+  echo "ta execute $TA_EXECUTE" > ta_exec.sh
+fi
+
 sh ta_exec.sh
